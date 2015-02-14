fbc4909d740b6c5846f72467fb91087b9f540f55
Kapu06 <kapu@riseup.net>
2015-02-14 17:25:41 +0100

    Ajout de main.c et structures.h

---
+++ Création type variable/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+
+
+int main()
+{
+int i=0;
+Personne joueur[i]; //définition variables joueurs type Personne
+
+for(i=0;i<2;i++)
+{
+ //Présentation du jeu
+ printf("\nBonjour à vous\nCe jeux se joue à deux joueurs maximum pour le moment.\nAvant de démarrer, il me faudrait quelques informations pour créer vos avatars.\n");
+ //Demande du prénom
+ printf("Quel est votre prénom joueur %d?:\t",i);
+ scanf("%s",joueur[i].prenom);
+ //Demande du nom
+ printf("\nQuel est votre nom?\t");
+ scanf("%s",joueur[i].nom);
+ //Demande du signe astrologique
+ printf("\nQuel est votre signe astrologique?\t");
+ scanf("%s",joueur[i].signe_astrologique);
+ //Demande de l'âge
+ /*printf("\nQuel âge avez-vous?\t");
+ scanf("%d", age);*/
+ /* Je ne vois pas encore comment faire de booléen ici
+ printf("Êtes-vous:\n\t1. Un garçon?\n\t2.Une Fille?\n (Répondez par '1' ou '2')\n");
+ scanf("%d",joueur[i].sexe);
+ */
+
+}
+
+for (i=0;i<2;i++)
+{
+    printf("Très bien joueur%d, voici tes informations:\nPrénom:%s\nNom:%s\nSigne astrologique:%s\n", i,joueur[i].prenom,joueur[i].nom,joueur[i].signe_astrologique);
+}
+
+
+
+    return 0;
+}
+
---
+++ Création type variable/structures.h
@@ -0,0 +1,17 @@
+#ifndef DEF_STRUCTURES
+#define DEF_STRUCURES
+
+typedef struct Personne Personne;
+struct Personne
+{
+
+ char nom[100],prenom[100],signe_astrologique[100];
+ int age,sexe;
+
+};
+
+
+
+#endif // DEF_STRUCTURES
+
+
