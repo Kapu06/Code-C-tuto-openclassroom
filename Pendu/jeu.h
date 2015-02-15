83ef5e75a3f4f564aa439f83be97a3936357517c
Kapu06 <kapu@riseup.net>
2015-02-15 23:29:32 +0100

    Ajout des fichiers main.c et jeu.h pour pouvoir les voir à distance

---
+++ Pendu/jeu.h
@@ -0,0 +1,18 @@
+#ifndef DEF_JEU
+#define DEF_JEU
+
+char lirecaractere()
+{
+    char caractere = 0;
+
+    caractere = getchar();//Lit le premier caractère
+    caractere = toupper(caractere);
+
+    while(getchar() != "\n");
+
+    return caractere;
+}
+
+
+
+#endif // DEF_JEU
