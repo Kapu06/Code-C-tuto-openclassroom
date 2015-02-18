c7df69003e644a04de313dbb3f26caa36710ddb9
Kapu06 <kapu@riseup.net>
2015-02-19 00:17:11 +0100

    a mis dans le .h les fonctions pour libérer le main, mais garde le bugs

--- Pendu/jeu.h
+++ Pendu/jeu.h
@@ -13,6 +13,42 @@ char lirecaractere()
     return caractere;
 }
 
+void masquageMotCahe(char pointeurMotSecret[],char pointeurMotCache[])
+{
+int longeurChaine=0, i=0;
+
+    longeurChaine=strlen(pointeurMotSecret);//calcul longeur nécessaire pour taille motCache
+
+*pointeurMotCache = malloc(longeurChaine * sizeof(char));//allocation dynamique mémoire
+if (pointeurMotCache == NULL)
+{
+    exit(0);//arrêt du programme si échec allocation mémoire
+}
+else
+{
+        for (i=0;i<longeurChaine;i++)//Calcul du nombre d'asterix à afficher: bug!
+
+{
+    pointeurMotCache[i]='*';
+}
+}
+}
+
+void verificationOuvertureFichier (FILE* fichierMotCache, char motCache[])
+{
+      if (fichierMotCache != NULL)
+ {
+    fprintf(fichierMotCache, "%s", motCache); //Ecriture dans le fichier motCache.txt du motCache (***)
+
+      fclose(fichierMotCache);
+ }
+
+ else
+ {
+     printf("Impossible d'ouvrir le fichier motCache.txt");
+ }
+}
+
 
 
 #endif // DEF_JEU
--- Pendu/main.c
+++ Pendu/main.c
@@ -7,54 +7,28 @@
 /* Se servir de motCache.txt pour y stocker les asterix -> les afficher et y modifier dedans le mot trouvé au fur et à mesure */
 /*Ecrit bien dans le fichier mais, rajoute un + à la fin; vider le fichier au
 début du programme!!; pb d'affichage nombre de coups et caractère saisi, réglé
-si retire la boucle for, mais pose pb de motCache=******  IDée: extérioriser cette
-boucle dans une fonction dans le header!!!!!*/
+si enlève le '*' à ligne 22 du pointeur, mais pose pb de motCache=****** */
 
 int main()
 {
-    int coupsRestant=10, i=0, compteurCoups=0, longeurChaine=0;
+    int coupsRestant=10,compteurCoups=0;
     char maLettre = 0, motSecret[]="MARRON",motCache[]="", *pointeurRecherche = NULL;
 
     FILE* fichierMotCache = NULL;
     fichierMotCache = fopen("motCache.txt","r+");
 
-longeurChaine=strlen(motSecret);//calcul longeur nécessaire pour taille motCache
-
-*motCache = malloc(longeurChaine * sizeof(char));//allocation dynamique mémoire
-if (motCache == NULL)
-{
-    exit(0);//arrêt du programme si échec allocation mémoire
-}
-
-for (i=0;i<=longeurChaine;i++)//Calcul du nombre d'asterix à afficher: bug!
-
-{
-    motCache[i]='*';
-}
-
-    if (fichierMotCache != NULL)
- {
-    fprintf(fichierMotCache, "%s", motCache); //Ecriture dans le fichier motCache.txt du motCache (***)
-
-      fclose(fichierMotCache);
- }
-
- else
- {
-     printf("Impossible d'ouvrir le fichier motCache.txt");
- }
-
-
+masquageMotCahe(motSecret,motCache);
+verificationOuvertureFichier(fichierMotCache, motCache);
 
 
 
 //Début de présentation du jeu
 printf("=========================\n\tLE PENDU\n=========================\n");
-printf("\n\nBienvenu dans ce jeu du pendu jeune aventurier.\nAvant de commencer je vais t'énumérer les quelques règles\n que tu devras respecter:");
+printf("\n\nBienvenu dans ce jeu du pendu jeune aventurier.\nAvant de commencer je vais t'énumérer les quelques règles\nque tu devras respecter:");
 printf("\n\t1.Tu devras trouver le mot mystère en moins de 10 coups sinon\n\tun innocent mourra.\n\t2.Tu ne pourras proposer qu'une et une seule lettre par tours.\n");
 printf("\n\nPrêt?....\n\n");
 
-while (coupsRestant !=0)
+while (coupsRestant >0)
 {
 
     printf("\n\nIl vous reste %d coups à jouer\n", coupsRestant);
@@ -82,11 +56,6 @@ while (coupsRestant !=0)
 }
 
 //Afficher message perdu jeu
-
-
-
-
-
 
     return 0;
 }
